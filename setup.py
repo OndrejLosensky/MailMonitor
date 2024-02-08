2daf6e9f4d3c913edd3f422976f1580de1aaf7b2
Ondrej Losensky <losenskyondrej@gmail.com>
2024-02-08 18:27:24 +0100

    MailMonitor

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+APP = ['Main.py']
+DATA_FILES = ['login.yaml', 'logo.png']  # Include necessary data files
+OPTIONS = {
+    'packages': ['rumps', 'yaml', 'imaplib', 'smtplib', 'email'],  # Add necessary packages
+    'iconfile': 'logo.png',  # Provide the path to the icon file in .icns format
+    'plist': {
+        'CFBundleDisplayName': 'UnreadEmails',  # Set the display name of your application
+        'CFBundleName': 'UnreadEmails',  # Set the bundle name of your application
+        'CFBundleIdentifier': 'com.yourcompany.YourAppName',  # Set the bundle identifier
+        'CFBundleVersion': "1.0.0",  # Set the version of your application
+        'LSUIElement': True,  # Set to True to create a menu bar app without a dock icon
+        'NSHumanReadableCopyright': 'Copyright 2024, YourCompany',
+        # Add any other necessary plist settings
+    }
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
